a5c3271ddb1249b3ec7652922b64638bc7711c8a
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-02-26 15:19:23 +0530

    Added cors validations to handle the frontend requests

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const cors = require("cors");
 const dbConnection = require("./connection");
 const routers = require("./router");
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/", routers);
 
